f4293d2dab7e89e06caaade4a0e2fe4b0a09d03a
Davis Lang <s3775656@student.rmit.edu.au>
2023-10-17 17:16:00 +1100

    damson

---
+++ public/pages/projectscripts.js
@@ -0,0 +1,54 @@
+const canvas = document.getElementById('background-canvas');
+const ctx = canvas.getContext('2d');
+const pixelSize = 5; // Size of each pixel
+
+// Set canvas dimensions to match the window size
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let lastTime = 0;
+const frameDelay = 100; // Delay between frames in milliseconds
+const fadeSpeed = 0.02; // Speed of fade-in effect
+
+// Create an array to store the opacity of each pixel
+const pixelOpacity = [];
+
+for (let x = 0; x < canvas.width; x += pixelSize) {
+    for (let y = 0; y < canvas.height; y += pixelSize) {
+        pixelOpacity.push(0); // Initialize the opacity of each pixel to 0
+    }
+}
+
+function generateNoise() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let x = 0, i = 0; x < canvas.width; x += pixelSize) {
+        for (let y = 0; y < canvas.height; y += pixelSize, i++) {
+            // Gradually increase the opacity of each pixel
+            if (pixelOpacity[i] < 1) {
+                pixelOpacity[i] += fadeSpeed;
+            }
+
+            const greyValue = Math.floor(Math.random() * 51); // Darker shades of grey
+            ctx.fillStyle = `rgba(${greyValue}, ${greyValue}, ${greyValue}, ${pixelOpacity[i]})`;
+            ctx.fillRect(x, y, pixelSize, pixelSize);
+        }
+    }
+}
+
+function updateNoise(timestamp) {
+    if (!lastTime) lastTime = timestamp;
+
+    const elapsed = timestamp - lastTime;
+
+    if (elapsed > frameDelay) {
+        generateNoise();
+        lastTime = timestamp;
+    }
+
+    requestAnimationFrame(updateNoise);
+}
+
+// Start the animation
+updateNoise(0);
