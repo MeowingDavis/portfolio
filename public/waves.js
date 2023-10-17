47d60f9b9effea64a52a71fbdb450b1915543655
Davis Lang <s3775656@student.rmit.edu.au>
2023-10-17 15:20:57 +1100

    stylin

--- public/waves.js
+++ public/waves.js
@@ -2,24 +2,32 @@ function createRandomColorTentacleBackground() {
     const canvas = document.getElementById("background-canvas");
     const ctx = canvas.getContext("2d");
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    // Function to update the canvas dimensions
+    function updateCanvasDimensions() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
 
-    const tentacleWidth = 100; // Adjust the width of the tentacles
-    const tentacleCount = 800; // Adjust the number of tentacles
-    const tentacleSpeed = 0.2; // Adjust the speed of the tentacles (slower)
-    const tentacleAmplitude = 100; // Adjust the amplitude of the tentacles
+    // Initial canvas dimensions
+    updateCanvasDimensions();
+
+    const tentacleWidth = 100;
+    const tentacleCount = 800;
+    const tentacleSpeed = 0.2;
+    const tentacleAmplitude = 100;
     const backgroundColor = "black";
 
-    ctx.imageSmoothingEnabled = true; // Disable smoothing for a pixelated effect
+    ctx.imageSmoothingEnabled = true;
 
-    let currentColor = getRandomRGBAColor(); // Initialize with a random color
+    let currentColor = getRandomRGBAColor();
 
     function drawBackground() {
+        updateCanvasDimensions(); // Update canvas dimensions when drawing
+
         ctx.fillStyle = backgroundColor;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        ctx.fillStyle = currentColor; // Set the fill color to the current color
+        ctx.fillStyle = currentColor;
 
         for (let i = 0; i < tentacleCount; i++) {
             const xPos = (i / tentacleCount) * canvas.width;
@@ -32,23 +40,22 @@ function createRandomColorTentacleBackground() {
         requestAnimationFrame(drawBackground);
     }
 
-    // Function to generate a random RGBA color
     function getRandomRGBAColor() {
         const r = Math.floor(Math.random() * 256);
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
-        const a = Math.random(); // Random alpha (transparency) value between 0 and 1
+        const a = Math.random();
         return `rgba(${r}, ${g}, ${b}, ${a})`;
     }
 
-    // Change the color when the button is clicked
     document.getElementById("color-change-button").addEventListener("click", function () {
         currentColor = getRandomRGBAColor();
     });
 
-    // Start the animation
     drawBackground();
 }
 
-// Call the function when the page loads
 window.addEventListener("load", createRandomColorTentacleBackground);
+
+// Add a window resize event listener to update canvas size
+window.addEventListener("resize", createRandomColorTentacleBackground);
