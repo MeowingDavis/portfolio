00666f873e8cc4b08d4dbc53c9b27ec8a68ca3a3
Davis Lang <s3775656@student.rmit.edu.au>
2023-10-17 21:29:48 +1100

    push

--- public/pages/projectscripts.js
+++ public/pages/projectscripts.js
@@ -2,9 +2,17 @@ const canvas = document.getElementById('background-canvas');
 const ctx = canvas.getContext('2d');
 const pixelSize = 5; // Size of each pixel
 
-// Set canvas dimensions to match the window size
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// Update canvas dimensions when the window is resized
+function updateCanvasSize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+// Call the updateCanvasSize function when the window is resized
+window.addEventListener('resize', updateCanvasSize);
+
+// Set initial canvas size when the script is first run
+updateCanvasSize();
 
 let lastTime = 0;
 const frameDelay = 100; // Delay between frames in milliseconds
@@ -14,42 +22,41 @@ const fadeSpeed = 0.02; // Speed of fade-in effect
 const pixelOpacity = [];
 
 for (let x = 0; x < canvas.width; x += pixelSize) {
-    for (let y = 0; y < canvas.height; y += pixelSize) {
-        pixelOpacity.push(0); // Initialize the opacity of each pixel to 0
-    }
+  for (let y = 0; y < canvas.height; y += pixelSize) {
+    pixelOpacity.push(0); // Initialize the opacity of each pixel to 0
+  }
 }
 
 function generateNoise() {
-    // Clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (let x = 0, i = 0; x < canvas.width; x += pixelSize) {
-        for (let y = 0; y < canvas.height; y += pixelSize, i++) {
-            // Gradually increase the opacity of each pixel
-            if (pixelOpacity[i] < 1) {
-                pixelOpacity[i] += fadeSpeed;
-            }
+  for (let x = 0, i = 0; x < canvas.width; x += pixelSize) {
+    for (let y = 0; y < canvas.height; y += pixelSize, i++) {
+      // Gradually increase the opacity of each pixel
+      if (pixelOpacity[i] < 1) {
+        pixelOpacity[i] += fadeSpeed;
+      }
 
-            const greyValue = Math.floor(Math.random() * 51); // Darker shades of grey
-            ctx.fillStyle = `rgba(${greyValue}, ${greyValue}, ${greyValue}, ${pixelOpacity[i]})`;
-            ctx.fillRect(x, y, pixelSize, pixelSize);
-        }
+      const greyValue = Math.floor(Math.random() * 51); // Darker shades of grey
+      ctx.fillStyle = `rgba(${greyValue}, ${greyValue}, ${greyValue}, ${pixelOpacity[i]})`;
+      ctx.fillRect(x, y, pixelSize, pixelSize);
     }
+  }
 }
 
 function updateNoise(timestamp) {
-    if (!lastTime) lastTime = timestamp;
+  if (!lastTime) lastTime = timestamp;
 
-    const elapsed = timestamp - lastTime;
+  const elapsed = timestamp - lastTime;
 
-    if (elapsed > frameDelay) {
-        generateNoise();
-        lastTime = timestamp;
-    }
+  if (elapsed > frameDelay) {
+    generateNoise();
+    lastTime = timestamp;
+  }
 
-    requestAnimationFrame(updateNoise);
+  requestAnimationFrame(updateNoise);
 }
-
 
 // Start the animation
 updateNoise(0);
